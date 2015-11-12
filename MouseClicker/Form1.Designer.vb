4a611dc47075477433fb288a120152dd9c346a15
deiesuone <deiesuone@gmail.com>
2015-11-12 15:57:39 +0900

    add

---
+++ MouseClicker/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(11, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "左クリック"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(92, 31)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "右クリック"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(11, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(90, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(11, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "0"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(32, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(135, 19)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "1000"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(11, 72)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(156, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "停止"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(11, 9)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(15, 14)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(179, 102)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "MouseClicker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ MouseClicker/Form1.vb
@@ -0,0 +1,93 @@
+﻿Public Class Form1
+    <System.Runtime.InteropServices.DllImport("USER32.DLL")> _
+    Private Shared Sub mouse_event(ByVal dwFlags As Integer, ByVal dx As Integer _
+                                   , ByVal dy As Integer, ByVal cButtons As Integer _
+                                   , ByVal dwExtraInfo As Integer)
+    End Sub
+
+    Private Const MOUSEEVENTF_LEFTUP As Integer = &H4
+    Private Const MOUSEEVENTF_LEFTDOWN As Integer = &H2
+    Private Const MOUSEEVENTF_RIGHTUP As Integer = &H10
+    Private Const MOUSEEVENTF_RIGHTDOWN As Integer = &H8
+
+    Private ltimer As New Timers.Timer
+    Private rtimer As New Timers.Timer
+
+    Private lCount As Integer = 0
+    Private rCount As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.ltimer.AutoReset = True
+        Me.ltimer.Interval = 1
+        AddHandler Me.ltimer.Elapsed, AddressOf MouseLeftClickDispacher
+
+        Me.rtimer.AutoReset = True
+        Me.rtimer.Interval = 1
+        AddHandler Me.rtimer.Elapsed, AddressOf MouseRightClickDispacher
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not Me.ltimer.Enabled Then
+            Me.lCount = 0
+            Me.Button1.Enabled = False
+            Me.ltimer.Start()
+        Else
+            Me.ltimer.Stop()
+            Me.Button1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Not Me.rtimer.Enabled Then
+            Me.rCount = 0
+            Me.Button2.Enabled = False
+            Me.rtimer.Start()
+        Else
+            Me.rtimer.Stop()
+            Me.Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.ltimer.Stop()
+        Me.rtimer.Stop()
+        Me.Button1.Enabled = True
+        Me.Button2.Enabled = True
+    End Sub
+
+    Private Delegate Sub MouseDispacher()
+
+    Private Sub MouseLeftClickDispacher()
+        Me.BeginInvoke(New MouseDispacher(AddressOf MouseLeftClick))
+    End Sub
+
+    Private Sub MouseRightClickDispacher()
+        Me.BeginInvoke(New MouseDispacher(AddressOf MouseRightClick))
+    End Sub
+
+    Private Sub MouseLeftClick()
+        Me.lCount += 1
+        Me.Label1.Text = Me.lCount.ToString
+        If Me.lCount = CInt(Me.TextBox1.Text) Then
+            Me.Button1.Enabled = True
+            Me.ltimer.Stop()
+        End If
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+    End Sub
+
+    Private Sub MouseRightClick()
+        Me.rCount += 1
+        Me.Label2.Text = Me.rCount.ToString
+        If Me.rCount = CInt(Me.TextBox1.Text) Then
+            Me.Button2.Enabled = True
+            Me.rtimer.Stop()
+        End If
+        mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        Me.TopMost = Me.CheckBox1.Checked
+    End Sub
+End Class
